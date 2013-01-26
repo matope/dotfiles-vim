d7571a699d6c31e8a3f87b65313ffa0fb698f6d4
matope <matope.ono@gmail.com>
2013-01-27 02:42:20 +0900

    install.shにgit submodule init/updateを追加

--- README.md
+++ README.md
@@ -7,7 +7,5 @@ Installation
 git clone git@github.com:matope/dotfiles-vim.git 
 #(or git://github.com/matope/dotfiles-vim.git)
 cd dotfiles-vim/
-git submodule init
-git submodule update
 ./install.sh
 ```
--- install.sh
+++ install.sh
@@ -7,13 +7,17 @@ success () {
 ask_and_link() {
   /bin/echo -n "Make link $1 -> $2 ? [y/n]"
   read ANS
-  if [ $ANS = 'y' -o $ANS = 'yes' ]; then
+  if [ "$ANS" = 'y' -o "$ANS" = 'yes' ]; then
     ln -fs $1 $2
     success "Linked $1 -> $2"
   fi
 }
 
 DOTFILES_ROOT=`cd $(dirname $0); pwd`
+
+cd $DOTFILES_ROOT
+git submodule init
+git submodule update
 
 ask_and_link $DOTFILES_ROOT/.vimrc $HOME/.vimrc
 ask_and_link $DOTFILES_ROOT/.vim $HOME/.vim
