4430c8b6d7231ff2fe7e3b3926079dcaa12c67e7
ono_matope <matope.ono@gmail.com>
2014-05-02 19:03:43 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,16 +4,6 @@ success () {
   printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
 }
 
-ask_and_link() {
-  ln -siv $1 $2
-#  /bin/echo -n "Make link $1 -> $2 ? [y/n]"
-#  read ANS
-#  if [ "$ANS" = 'y' -o "$ANS" = 'yes' ]; then
-#    ln -fs $1 $2
-#    success "Linked $1 -> $2"
-#  fi
-}
-
 DOTFILES_ROOT=`cd $(dirname $0); pwd`
 
 cd $DOTFILES_ROOT
@@ -21,9 +11,9 @@ git submodule init
 git submodule foreach 'git pull origin master'
 git submodule update
 
-ask_and_link $DOTFILES_ROOT/.vimrc $HOME/.vimrc
-ask_and_link $DOTFILES_ROOT/.vimrc.powerline $HOME/.vimrc.powerline
-ask_and_link $DOTFILES_ROOT/.vim $HOME/.vim
+ln -siv $DOTFILES_ROOT/.vimrc $HOME/.vimrc
+ln -siv $DOTFILES_ROOT/.vimrc.powerline $HOME/.vimrc.powerline
+ln -siv $DOTFILES_ROOT/.vim $HOME/.vim
 
 success "Link done. executing vim +\":BundleInstall\"..."
 vim +":BundleInstall" +:q +:q
