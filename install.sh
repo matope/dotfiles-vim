aac27e6c4e0b814fd9970b1ab9316645edfbfcf2
Yasuharu Goto <matope.ono@gmail.com>
2016-06-29 20:22:32 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ ln -siv $DOTFILES_ROOT/.vim $HOME/.vim
 
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-success "Link done. executing vim +\":PlugInstall\"..."
+echo "Link done. executing vim +\":PlugInstall\"..."
 vim +":PlugInstall" +:q +:q
 
 exit 0
